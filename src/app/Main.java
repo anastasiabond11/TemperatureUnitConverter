edb8c1d61ae8b6e34451bd304db600035187d901
Anastasiia Bondarenko <bondarenkonastya02@icloud.com>
2024-06-14 16:50:25 +0200

    Add conversion of fahrenheits to celsius

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,5 +6,13 @@ public class Main {
 
         System.out.println("Temperature unit converter");
 
+        double fahrenheit = 75;
+        double convCelsius = convertFahrenheitToCelsius(fahrenheit);
+        System.out.printf("\n%.2f fahrenheit is equivalent to %.2f celsius", fahrenheit, convCelsius);
+    }
+
+    public static double convertFahrenheitToCelsius(double fahrenheit){
+        double celsius = (fahrenheit - 32) * 5/9;
+        return celsius;
     }
 }
